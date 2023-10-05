feb73713d8632a83eee96fd6bf7827b9d9e27eb5
= <rdd57@case.edu>
2023-10-05 11:54:46 -0400

    mongodb connection

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,6 +1,15 @@
 from flask import Flask
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+client = MongoClient('mongodb+srv://svb24:soccersphere@soccersphere.wr8cfd3.mongodb.net/', username='svb24', password='soccersphere')
+
+## Example: pulling data from a random match
+db = client["matches"]
+col = db['106']
+x = col.find_one()
+print(x)
 
 # test API route
 @app.route("/test")
