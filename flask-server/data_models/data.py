6f437889741efe1d5abca5c50dd82c31790cd24b
= <rdd57@case.edu>
2023-10-12 10:56:11 -0400

    endpoints for matches and event player locations

--- flask-server/data_models/data.py
+++ flask-server/data_models/data.py
@@ -1,5 +1,6 @@
 '''Contains main processing and queries from MongoDB'''
 from pymongo import MongoClient
+from bson.json_util import dumps
 
 class Data:
     '''Class contains most major queries to access larger data and collections from MongoDB'''
@@ -12,7 +13,7 @@ class Data:
         ## check if all databases exist
         dblist = self.client.list_database_names()
         if set(dblist) >= set(['matches', 'lineups', 'events', 'three-sixty']):
-            print("MongoDB client connected") 
+            print("MongoDB client connected")
 
         ## get the databases
         self.matches = self.client['matches']
@@ -30,20 +31,39 @@ class Data:
         self.lineups = self.client['lineups']
         self.events = self.client['events']
         self.threesixty = self.client['three_sixty']
+        
+    def get_matches(self):
+        '''returns all matches'''
+        return self.matches['106'].find({}, {"_id": 0})
 
     def get_match(self, match_id):
         '''returns match document given a match id'''
-        return self.matches['106'].find({"match_id": match_id})[0]
+        return self.matches['106'].find_one({"match_id": int(match_id)}, {"_id": 0})
 
     def get_lineups(self, match_id):
         '''return lineups (lineups are 2 documents per collection)'''
-        return self.lineups[match_id]
+        return self.lineups[match_id].find({},{"_id": 0})
 
-    def get_events(self, match_id):
+    def get_events(self, match_id, remove={}):
         '''return the events for a particular game (returns list of documents)'''
-        return self.events[match_id]
+        return self.events[match_id].find({},{"_id": 0})
+
+    def get_event_player_data(self, match_id):
+        '''
+            Returns event data with corresponding player location data in form
+            {event: {player_id: [x, y], player_id: [x, y]}, event: ...}
+        '''
+        events = self.get_events(match_id, remove={})[:10]
+        threesixty_data = self.threesixty[match_id]
+        player_event_data = {}
+        for event in events:
+            event_id = event['id']
+            location_data = threesixty_data.find_one({'event_uuid': event_id}, {"_id": 0})
+            if location_data is not None:
+                location_data = location_data['freeze_frame']
+            player_event_data[event_id] = {'event_info': event, 'player_locations': location_data}
+        return player_event_data
 
     def get_threesixty(self, match_id):
         '''return the three sixty data for a particular game'''
-        return self.threesixty[match_id]
-    
+        return self.threesixty[match_id].find({},{"_id": 0})
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,21 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from pymongo import MongoClient
 from data_models.data import Data
+from bson.json_util import dumps
 
 app = Flask(__name__)
 
 data = Data()
-match_data = data.get_match(3835335)
-team_name = match_data['home_team']['home_team_name']
-
+match_data = data.get_matches()
 
 # test API route
 @app.route("/test")
 def test():
-    output = f"<h1> {str(team_name)} <h1>"
-    print(output)
     return {"test": ["test1", "test2", "test3"]}
-    
+
+## get the data of all available matches
+@app.route("/matches/", methods=["GET"])
+def get_matches():
+    matches = {match['match_id']:match for match in data.get_matches()}
+    for match in data.get_matches():
+        matches[match['match_id']] =  match
+    return jsonify(matches)
+
+## get the match ids of available matches
+@app.route('/match-ids/', methods=["GET"])
+def get_match_ids():
+    match_ids = [str(match['match_id']) for match in data.get_matches()]
+    return {"matches": match_ids}
+
+## get the data of a match by the id
+@app.route("/matches/<match_id>", methods=["GET"])
+def get_match(match_id):
+    match_data = data.get_match(match_id)
+    return jsonify(match_data)
+
+@app.route("/event_players/<match_id>", methods=["GET"])
+def get_event_players(match_id):
+    event_player_data = data.get_event_player_data(match_id)
+    return jsonify(event_player_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
